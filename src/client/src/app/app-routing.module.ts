91fd062d11af3f52b6a392584c6e5ee11a510734
sabiyoung <ethiodatecakes@gmail.com>
2021-10-05 16:07:10 -0700

    feat(app.routing): add path

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserInputComponent } from './components/user-input/user-input.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
 import { PageUsersComponent } from './pages/page-users/page-users.component';
 
 const routes: Routes = [
-  {path: 'users', component: PageUsersComponent}
+  {path: 'users', component: PageUsersComponent},
+  {path: '', component: UserInputComponent}
 ];
 
 @NgModule({

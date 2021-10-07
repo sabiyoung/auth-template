23c029a0d32b6c0b9b962c13867300fb3c8cb5a6
sabiyoung <ethiodatecakes@gmail.com>
2021-10-07 16:59:05 -0700

    feat(app.routing) add routing path

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserInputComponent } from './components/user-input/user-input.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
-import { PageUsersComponent } from './pages/page-users/page-users.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
+import { UserPostPageComponent } from './pages/user-post-page/user-post-page.component';
+
 
 const routes: Routes = [
-  {path: 'users', component: PageUsersComponent},
-  {path: '', component: UserInputComponent}
+  {path: 'users', component: UserPageComponent},
+   {path: 'sign-in', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'posts', component: UserPostPageComponent},
+  {path: '**', redirectTo:" posts"}
+
+
 ];
 
 @NgModule({

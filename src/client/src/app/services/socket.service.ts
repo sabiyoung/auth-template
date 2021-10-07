acb708f54defb779da1bfda2fed5b788796b7b86
sabiyoung <ethiodatecakes@gmail.com>
2021-10-07 16:54:04 -0700

    feat(service): add socket service

---
+++ src/client/src/app/services/socket.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+import * as io from 'socket.io-client';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+socket: any;
+readonly uri: string =  "ws://localhost:3501";
+  constructor(
+    ) {
+this.socket = io.io(this.uri)
+  }
+
+  listen(eventName: string) {
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, (data: any) => {
+        subscriber.next(data)
+      })
+    })
+  }
+
+  emit(eventName: string, data: any) {
+    this.socket.emit(eventName, data)
+  }
+}

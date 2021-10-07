d969c9340a7e1b37717738cd01efd27776ee286c
sabiyoung <ethiodatecakes@gmail.com>
2021-10-07 16:58:12 -0700

    feat(componenet): add register

---
+++ src/client/src/app/pages/register-page/register-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store';
+import { usersSelector, selectedUserSelector } from 'src/app/store/selectors/user/user.selectors';
+import { User } from '../../../../../shared/models/user.model';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.scss']
+})
+export class RegisterPageComponent implements OnInit {
+
+
+  users$: Observable<User[]>;
+  selectedUser$: Observable<User | null>;
+  constructor(
+    private store: Store<AppState>,
+  ) {
+    this.users$ = this.store.select(usersSelector);
+    this.selectedUser$ = this.store.select(selectedUserSelector);
+   }
+
+
+  ngOnInit(): void {
+  }
+
+}

9079a474ebafefdb3ee2f10e12d57e7278730dbb
sabiyoung <ethiodatecakes@gmail.com>
2021-10-07 17:08:35 -0700

    feat(user.model): add id & tweet property

--- src/shared/models/user.model.ts
+++ src/shared/models/user.model.ts
@@ -1,6 +1,8 @@
 import * as mongoose from 'mongoose';
 export interface User {
     _id?:{type: mongoose.Types.ObjectId}
+    id?: string,
+    tweet?: string,
     name: string,
     username: string,
     email: string,

82be9159c1c045c383a47e0e17feda5c12ffb99a
sabiyoung <ethiodatecakes@gmail.com>
2021-10-15 11:20:24 -0700

    add html & & style in tweet & comment components

--- src/client/src/app/pages/user-post-page/user-post-page.component.ts
+++ src/client/src/app/pages/user-post-page/user-post-page.component.ts
@@ -5,10 +5,10 @@ import { Observable } from 'rxjs';
 import { SocketService } from 'src/app/services/socket.service';
 import { UserService } from 'src/app/services/user.service';
 import { AppState } from 'src/app/store';
-import { UserTweet } from 'src/app/store/actions/user/user.actions';
-import { UserTweetSelector } from 'src/app/store/selectors/user/user.selectors';
+import { createComment, createTweet, loadComments, loadTweets, updateDislikes, updateLikes } from 'src/app/store/actions/user/user.actions';
+import { CommentSelector, UserTweetSelector } from 'src/app/store/selectors/user/user.selectors';
+import { Comment } from '../../../../../shared/models/comment.model';
 import { Tweet } from '../../../../../shared/models/tweet.model';
-import { User } from '../../../../../shared/models/user.model';
 
 @Component({
   selector: 'app-user-post-page',
@@ -16,29 +16,48 @@ import { User } from '../../../../../shared/models/user.model';
   styleUrls: ['./user-post-page.component.scss']
 })
 export class UserPostPageComponent implements OnInit {
-  tweets$: Observable<Tweet[]> = new Observable((observer) => {
-    observer.next([])
-  })
-  constructor(
-    private socketService: SocketService,
-    private userService: UserService,
-    private store: Store<AppState>
+  tweets$:Observable<Tweet[]>
+  comments$:Observable<Comment[]>
+   constructor(
+     private userService: UserService,
+     private store: Store<AppState>,
+     private socketService: SocketService
+   ) {
+     this.store.dispatch(loadTweets())
+     this.store.dispatch(loadComments());
+     this.tweets$ = this.store.select(UserTweetSelector);
+     this.comments$ = this.store.select(CommentSelector)
+    }
 
-  ) {
-
-   }
 
   ngOnInit(): void {
     this.socketService.listen('user tweet').subscribe((data) => {
       console.log(data)
     })
+
   }
 
-  get tweets() {
-    return this.tweets$
-  }
+
   createTweet(text: string) {
- this.userService.createTweets( {text}).subscribe()
+//  this.userService.createTweets( {text}).subscribe();
+ this.store.dispatch(createTweet({data:{text}}))
 
   }
+
+  onIncrement(tweet: Tweet) {
+    this.store.dispatch(updateLikes({data:tweet}))
+    // console.log(this.onIncrement)
+
+  }
+
+  onDecrement(tweet: Tweet) {
+    this.store.dispatch(updateDislikes({data:tweet}))
+    // console.log(this.onIncrement)
+  }
+  createComment(text: string, tweet: Tweet) {
+    // this.userService.createComments({text, tweet}).subscribe()
+this.store.dispatch(createComment({data:{text: text, tweet: tweet}}))
+     }
+
+
 }

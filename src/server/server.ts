ff42f83d108d8f1f636b15622bb697aadd7d7860
sabiyoung <ethiodatecakes@gmail.com>
2021-10-25 08:44:30 -0700

    feat(server) add img upload medthod

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,10 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import cors from "cors";
 import { PostModel } from "./schemas/post.schema.js";
 import { UserModel } from "./schemas/user.schema.js";
 import mongoose from "mongoose";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { authHandler } from "./middleware/auth.middleware.js";
@@ -15,60 +15,151 @@ import { TweetModel } from "./schemas/tweet.schema.js";
 import { CommentModel } from "./schemas/comment.schema.js";
 import multer from "multer";
 import { GridFsStorage } from "multer-gridfs-storage";
-import crypto from "crypto";
-import path from "path";
+import bodyParser from "body-parser";
 dotenv.config();
 const access_secret = process.env.ACCESS_TOKEN_SECRET as string;
 console.log(access_secret);
 
-
 const saltRounds = 10;
-
 const app = express();
 const PORT = 3502;
 let gfs;
+
 const mongoURI = "mongodb://localhost:27017/test";
 mongoose
   .connect(mongoURI)
-  .then(() => {
+  .then(async () => {
     console.log("Connected to DB Successfully");
     gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
       bucketName: "uploads",
     });
+
+    const userOne = await getUserByName("Saba Abera");
+    // console.log(userOne)
+
+    const userTwo = await getUserByName("Ziva");
+    // console.log(userTwo);
+    // const updatedUserTwo = await followUser(userTwo?._id, userOne?._id);
+
+    // const aggr = await aggregate()
+    // console.log(aggr)
+
+    const followers = await whoIsFollowingMe(userOne?._id);
+    console.log(JSON.stringify(followers, null, 4));
   })
+
   .catch((err) => console.log("Failed to Connect to DB", err));
-// Storage
 const storage = new GridFsStorage({
   url: mongoURI,
   file: (req, file) => {
     return new Promise((resolve, reject) => {
-        const fileInfo = {
-          filename: file.originalname,
-          bucketName: "uploads",
-        };
-        resolve(fileInfo);
-
+      const fileInfo = {
+        filename: file.originalname,
+        bucketName: "uploads",
+      };
+      resolve(fileInfo);
     });
   },
 });
 
-const upload = multer({
-  storage,
-});
-app.use(cookieParser())
-app.use(cors({
+const upload = multer({ storage: storage });
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(
+  cors({
     credentials: true,
-    origin: ['http://localhost:4200', 'http://localhost:3502', 'http://localhost:8080']
-}));
+    origin: [
+      "http://localhost:4200",
+      "http://localhost:3502",
+      "http://localhost:8080",
+    ],
+  })
+);
 app.use(express.json());
 
 app.get("/", function (req, res) {
   res.json({ message: "test" });
 });
 
-app.post("/upload", upload.single("file"), (req, res) => {
+app.post("/file", upload.single("file"), (req, res, next) => {
   res.redirect("/");
+  const file = req.file;
+  console.log(file!.filename);
+  if (!file) {
+    throw new Error();
+  }
+  res.send(file);
 });
+// const db = '';
+// app.get("/file", (req, res) => {
+//   db.collection('uploads').find( {}).toArray((err: any, result:any) => {
+//    if(err) throw err
+//    res.send(result)
+//  })
+// })
+function getUserByName(name: string) {
+  return UserModel.findOne({ name }).populate("following");
+}
+
+function whoIsFollowingMe(_id: any) {
+  return UserModel.find({ following: _id }).count();
+}
+
+app.get("/get-all-users", function (req, res) {
+  UserModel.find()
+    .then((data) => res.json({ data }))
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
+    });
+});
+
+function followUser(userId: any, followId: any) {
+  // console.log(userId, 'sabaId', followId)
+  return UserModel.findOneAndUpdate(
+    { _id: userId },
+    {
+      $addToSet: { following: followId },
+    },
+    { new: true }
+  ).populate("following");
+}
+app.put("/update-following/:id", function (req, res) {
+  console.log("Update following");
+  UserModel.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: { following: req.body.followig },
+    },
+    {
+      new: true,
+    },
+    function (err, updateFollowing) {
+      if (err) {
+        res.send("Error user following");
+      } else {
+        res.json(updateFollowing);
+      }
+    }
+  );
+});
+// function aggregate() {
+//   return UserModel.aggregate(
+//     [{
+//       $project: {
+//         _id: 1,
+//         followers: '$following',
+//       }
+//     }, {
+//       $project: {
+//         _id: null,
+//         totalFollowers: {$sum: '$following'}
+//       }
+//     }
+//   ]
+//   )
+// }
 
 app.get("/posts", function (req, res) {
   PostModel.find()
@@ -80,8 +171,8 @@ app.get("/posts", function (req, res) {
 });
 
 ///////
-app.get("/tweets",function (req, res) {
-TweetModel.find()
+app.get("/tweets", function (req, res) {
+  TweetModel.find()
     .then((data) => res.json({ data }))
     .catch((err) => {
       res.status(501);
@@ -90,102 +181,77 @@ TweetModel.find()
 });
 
 app.post("/create-tweet", authHandler, function (req: any, res) {
- 
-      const {text, img, _id, likes, disLikes } = req.body;
+  const { text, img, _id, likes, disLikes } = req.body;
   const newTweet = new TweetModel({
- text,
- user: req.user._id
+    text,
+    user: req.user._id,
   });
- console.log(newTweet, '*')
- newTweet
+  console.log(newTweet, "*");
+  newTweet
     .save()
     .then((data) => {
       res.json({ data });
     })
     .catch((err) => {
-      console.log(err)
+      console.log(err);
       res.status(403);
       res.json({ errors: err });
     });
-
-   });
-  //////
-///// comments
-  app.get("/comments",function (req, res) {
-    CommentModel.find()
-        .then((data) => res.json({ data }))
-        .catch((err) => {
-          res.status(501);
-          res.json({ errors: err });
-        });
-    });
-    
-    app.post("/create-comment", authHandler, function (req: any, res) {
-     
-          const {text, img, tweet} = req.body;
-      const newComment = new CommentModel({
-     text,
-     tweetID: tweet._id
-      });
-     console.log(newComment, '*')
-     newComment
-        .save()
-        .then((data) => {
-          res.json({ data });
-        })
-        .catch((err) => {
-          console.log(err)
-          res.status(501);
-          res.json({ errors: err });
-        });
-    
-       });
-       
-/////
-  app.put("/increment-tweet-like/:id", function (req, res) {
-    console.log("Update user like");
-  TweetModel.findByIdAndUpdate(
-      req.params.id,
-      {
-        $inc: { likes: 1 },
-      },
-      {
-        new: true,
-      },
-      function (err, updateLike) {
-        if (err) {
-          res.send("Error liking user");
-        } else {
-          res.json(updateLike);
-        }
-      }
-    );
-  });
-  
-  app.put("/decrement-tweet-like/:id", function (req, res) {
-    console.log("Update user dislike");
-  TweetModel.findByIdAndUpdate(
-      req.params.id,
-      {
-        $inc: { disLikes: 1 },
-      },
-      {
-        new: true,
-      },
-      function (err, updateLike) {
-        if (err) {
-          res.send("Error liking user");
-        } else {
-          res.json(updateLike);
-        }
-      }
-    );
-  });
+});
 
 //////
+///// comments
+app.get("/comments", function (req, res) {
+  CommentModel.find()
+    .then((data) => res.json({ data }))
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
+    });
+});
+
+app.post("/create-comment", authHandler, function (req: any, res) {
+  const { text, img, tweet } = req.body;
+  const newComment = new CommentModel({
+    text,
+    tweetID: tweet._id,
+  });
+  console.log(newComment, "*");
+  newComment
+    .save()
+    .then((data) => {
+      res.json({ data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(501);
+      res.json({ errors: err });
+    });
+});
+
+/////
+app.put("/increment-tweet-like/:id", function (req, res) {
+  console.log("Update user like");
+  TweetModel.findByIdAndUpdate(
+    req.params.id,
+    {
+      $inc: { likes: 1 },
+    },
+    {
+      new: true,
+    },
+    function (err, updateLike) {
+      if (err) {
+        res.send("Error liking user");
+      } else {
+        res.json(updateLike);
+      }
+    }
+  );
+});
 
 app.get("/users", authHandler, function (req: any, res) {
-  UserModel.find({email: req.user.email}, '-password')
+  UserModel.find({ email: req.user.email }, "-password")
     .then((data) => res.json({ data }))
     .catch((err) => {
       res.status(501);
@@ -193,16 +259,16 @@ app.get("/users", authHandler, function (req: any, res) {
     });
 });
 app.post("/create-user", function (req, res) {
-   const { name, email, username, password } = req.body;
-
+  const { name, email, username, password, followers, following } = req.body;
+  console.log(req.body);
   bcrypt.genSalt(saltRounds, function (err, salt) {
-    bcrypt.hash(password, salt, function(error, hash) {
-      const {name, email, username, password} = req.body;
+    bcrypt.hash(password, salt, function (error, hash) {
       const user = new UserModel({
         name,
         username,
         email,
-        password: hash
+        following: [],
+        password: hash,
       });
       user
         .save()
@@ -210,16 +276,13 @@ app.post("/create-user", function (req, res) {
           res.json({ data });
         })
         .catch((err) => {
+          console.log(err);
           res.status(501);
           res.json({ errors: err });
         });
-    })
-   
-  
+    });
   });
 });
-
-
 
 app.post("/create-post", function (req, res) {
   const { title, body } = req.body;
@@ -266,22 +329,30 @@ app.put("/update-user/:id", function (req, res) {
   );
 });
 
+app.get("/logout", authHandler, function (req, res) {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    maxAge: 60 * 1000,
+  });
+  res.json({ message: "Successfully Logged out" });
+});
+
 app.post("/login", function (req, res) {
   const { email, password } = req.body;
 
   UserModel.findOne({ email })
     .then((user) => {
-        console.log(user);
-      
+      console.log(user);
+
       bcrypt.compare(password, `${user?.password}`, function (err, result) {
         if (result) {
           console.log("It matches!");
-          const accessToken = jwt.sign({user}, access_secret)
-          res.cookie('jwt', accessToken, {
-              httpOnly: true,
-              maxAge: 60 * 60 * 1000,
-          })
-          res.json({message: 'Successfully Logged In'})
+          const accessToken = jwt.sign({ user }, access_secret);
+          res.cookie("jwt", accessToken, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 1000,
+          });
+          res.json({ message: "Successfully Logged In" });
         } else {
           res.sendStatus(403);
         }
@@ -292,20 +363,16 @@ app.post("/login", function (req, res) {
     });
 });
 
-
-
-
 const server = createServer(app);
-let io = new Server(server,{
-    cors: {origin: ['http://localhost:4200']}
-  });
+let io = new Server(server, {
+  cors: { origin: ["http://localhost:4200"] },
+});
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-socket.emit('user tweet', "here is my tweet") 
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.emit("user tweet", "here is my tweet");
 });
 
 server.listen(PORT, function () {
   console.log(`starting at localhost http://localhost:${PORT}`);
 });
-

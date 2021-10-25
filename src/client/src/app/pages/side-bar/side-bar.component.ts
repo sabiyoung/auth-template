aecbb3bef5a1d640a45d4ec74ad81193090cc7e0
sabiyoung <ethiodatecakes@gmail.com>
2021-10-25 08:46:06 -0700

    feat(component) add logout method

--- src/client/src/app/pages/side-bar/side-bar.component.ts
+++ src/client/src/app/pages/side-bar/side-bar.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store';
+import { loadUsers, logoutUser } from 'src/app/store/actions/user/user.actions';
+import { usersSelector } from 'src/app/store/selectors/user/user.selectors';
+import { User } from '../../../../../shared/models/user.model';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,10 +13,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  users$: Observable<User[]>
+  constructor(
+    private store: Store<AppState>,
+    private router: Router) {
+    this.users$ = this.store.select(usersSelector);
   }
 
+  ngOnInit(): void {
+    this.store.dispatch(loadUsers());
+  }
+  logout() {
+    this.store.dispatch(logoutUser())
+  }
+ home() {
+    this.router.navigate(['home'])
+  }
 }

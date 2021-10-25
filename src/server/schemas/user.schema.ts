f801c9aceb281859555f640d768e6713d4cd9abb
sabiyoung <ethiodatecakes@gmail.com>
2021-10-25 08:49:05 -0700

    feat(schema) add properties

--- src/server/schemas/user.schema.ts
+++ src/server/schemas/user.schema.ts
@@ -7,6 +7,16 @@ const userSchema = new Schema<User>({
     username: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
+  friends: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+   following: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  
 })
 
 export const UserModel = model<User>('User',userSchema)
+
+userSchema.virtual('followers', {
+    ref: 'User',
+    localField: '_id',
+    foreignField: 'following',
+
+});
